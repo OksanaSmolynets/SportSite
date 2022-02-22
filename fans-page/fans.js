f869f14590d9185e48b9911fb119f00ed094e2b6
Oksana Smolynets <oksana.mavdryk25@gmail.com>
2022-02-22 02:18:05 +0200

    Dom tasks refactor

--- fans-page/fans.js
+++ fans-page/fans.js
@@ -8,35 +8,37 @@ getSendButton.addEventListener("click", function (e) {
     e.stopPropagation();
 
     required();
-
-    const createFanArticle = document.createElement('article');
-    createFanArticle.className = 'article article-fans';
-    const createFanMessage = document.createElement('p');
-    createFanMessage.innerHTML = getTextArea.value
-    createFanArticle.appendChild(createFanMessage);
-    getFansMessageContainer.before(createFanArticle);
-
-    const createDataContainer = document.createElement('div');
-    createDataContainer.className = 'article-fan-info';
-    createFanArticle.appendChild(createDataContainer);
-    const createCurrentData = document.createElement('p');
-    const today = new Date();
-    var date = today.getDate() + '.' + (today.getMonth() + 1) + '.' + today.getFullYear() + '   ' + today.getHours() + ":" + today.getMinutes();
-
-    createCurrentData.innerHTML = date
-    createDataContainer.appendChild(createCurrentData);
-    const createCurrentUserName = document.createElement('p');
-    createCurrentUserName.innerHTML = `Some Fan ${today.getFullYear()}`
-    createDataContainer.appendChild(createCurrentUserName);
-
     getTextArea.value = '';
 });
 
 function required() {
     var empt = getTextArea.value.trim().length;
+    var errorMessageElem = document.querySelector('.ErrorForm')
     console.log(empt)
     if (empt === 0) {
-        alert("Please input a Value");
-        return false;
+        errorMessageElem.style.display = 'block';
+    } else {
+        const createFanArticle = document.createElement('article');
+        createFanArticle.className = 'article article-fans';
+        const createFanMessage = document.createElement('p');
+        createFanMessage.innerHTML = getTextArea.value
+        createFanArticle.appendChild(createFanMessage);
+        getFansMessageContainer.before(createFanArticle);
+
+        const createDataContainer = document.createElement('div');
+        createDataContainer.className = 'article-fan-info';
+        createFanArticle.appendChild(createDataContainer);
+        const createCurrentData = document.createElement('p');
+        const today = new Date();
+        var date = today.getDate() + '.' + (today.getMonth() + 1) + '.' + today.getFullYear() + '   ' + today.getHours() + ":" + today.getMinutes();
+
+        createCurrentData.innerHTML = date
+        createDataContainer.appendChild(createCurrentData);
+        const createCurrentUserName = document.createElement('p');
+        createCurrentUserName.innerHTML = `Some Fan ${today.getFullYear()}`
+        createDataContainer.appendChild(createCurrentUserName);
+        const createHr = document.createElement('hr');
+        createFanArticle.appendChild(createHr);
+        errorMessageElem.style.display = 'none'
     }
 }
